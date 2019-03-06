7e6f2bbab5024a511bf6f4832807018e3f6f67d7
xAm64 <48089283+xAm64@users.noreply.github.com>
2019-03-06 10:55:32 +0100

    Ajout variable exercice livraison

--- MesExercices/Gestion livraison/Program.cs
+++ MesExercices/Gestion livraison/Program.cs
@@ -14,19 +14,19 @@ namespace Gestion_livraison
         {
             string[] num_client = new string[]
             {
-                "43A", "54A", "62B", "74B", "85B", "93C", "27C", "33D", "45D", "56F",
+                "43A", "54A", "62B", "74B", "85B", "93C", "27C", "33D", "45D", "56F", "64B",
             };
             string[] noms = new string[]
             {
-                "Aristide Barnit", "Joseph Loseille", "Léon Nidas", "Gaston Choconnou", "Louise Chimelle", "Justin Bribou", "Nicolas Metrel", "Léonite Caravane", "Albert Etbasque", "Jules Moche",
+                "Aristide Barnit", "Joseph Loseille", "Léon Nidas", "Gaston Choconnou", "Louise Chimelle", "Justin Bribou", "Nicolas Metrel", "Léonite Caravane", "Albert Etbasque", "Jules Moche", "Etchgaray Manex",
             };
             int[] jour = new int[]
             {
-                6, 6, 1, 6, 1, 2, 1, 6, 6, 1,
+                6, 6, 1, 6, 1, 2, 1, 6, 6, 1, 0,
             };
             int[] mois = new int[]
             {
-                8, 7, 9, 8, 7, 0, 0, 8, 7, 9,
+                8, 7, 9, 8, 7, 0, 0, 8, 7, 9, 11,
             };
             string[] days = new string[]
             {
@@ -54,15 +54,25 @@ namespace Gestion_livraison
                     Console.WriteLine("Le numéro corespondant a été trouvé");
                     int numJour = jour[index];
                     int numMois = mois[index];
+                    string txtJour;
+                    string txtMois;
                     if(numMois == 0)
                     {
-                        Console.WriteLine("Numero {0} Nom : {1} Pas de livraison le {2} Livraison possible toute l'année", saisie, noms[index], days[numJour]);
+                        txtMois = "livraison possible toute l'année";
                     }
                     else
                     {
-                        Console.WriteLine("Numero {0} Nom : {1} Pas de livraison le {2} ni au mois de {3}", saisie, noms[index], days[numJour], month[numMois]);
+                        txtMois = "Pas de livraison possible au mois de: " +month[numMois];
                     }
-                    
+                    if(numJour == 0)
+                    {
+                        txtJour = "livraison possible toute la semaine sauf le dimanche";
+                    }
+                    else
+                    {
+                        txtJour = "Pas de livraison le: " + days[numJour] +"ni le dimanche";
+                    }
+                    Console.WriteLine("Numero {0} Nom : {1}. {2}. {3}.", saisie, noms[index], txtJour, txtMois);
 
                 }
                 else//non trouvé
