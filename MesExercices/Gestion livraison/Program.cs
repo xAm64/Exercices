e98491d71ba0914a9e6ffa7e9081bd355306387e
xAm64 <48089283+xAm64@users.noreply.github.com>
2019-03-06 10:27:18 +0100

    Exercice livraison fini

---
+++ MesExercices/Gestion livraison/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion_livraison
+{
+    class Program
+    {
+        static string saisie;
+
+        static void Main(string[] args)
+        {
+            string[] num_client = new string[]
+            {
+                "43A", "54A", "62B", "74B", "85B", "93C", "27C", "33D", "45D", "56F",
+            };
+            string[] noms = new string[]
+            {
+                "Aristide Barnit", "Joseph Loseille", "Léon Nidas", "Gaston Choconnou", "Louise Chimelle", "Justin Bribou", "Nicolas Metrel", "Léonite Caravane", "Albert Etbasque", "Jules Moche",
+            };
+            int[] jour = new int[]
+            {
+                6, 6, 1, 6, 1, 2, 1, 6, 6, 1,
+            };
+            int[] mois = new int[]
+            {
+                8, 7, 9, 8, 7, 0, 0, 8, 7, 9,
+            };
+            string[] days = new string[]
+            {
+                "Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi",
+            };
+            string[] month = new string[]
+            {
+                "Aucun", "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre",
+            };
+
+            bool restart = false;
+            do
+            {
+                Console.WriteLine("Numéro du client");
+                saisie = Console.ReadLine();
+
+                int index = 0;
+                while ((index < num_client.Length)&&(saisie != num_client[index]))
+                {
+                    index++;
+                }
+                if(index < num_client.Length)//condition trouvé
+                {
+
+                    Console.WriteLine("Le numéro corespondant a été trouvé");
+                    int numJour = jour[index];
+                    int numMois = mois[index];
+                    if(numMois == 0)
+                    {
+                        Console.WriteLine("Numero {0} Nom : {1} Pas de livraison le {2} Livraison possible toute l'année", saisie, noms[index], days[numJour]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numero {0} Nom : {1} Pas de livraison le {2} ni au mois de {3}", saisie, noms[index], days[numJour], month[numMois]);
+                    }
+                    
+
+                }
+                else//non trouvé
+                {
+                    Console.WriteLine("Ce client n'existe pas !");
+                    Console.ReadLine();
+                }
+                Console.WriteLine("Un autre essaie ?");
+                restart = Console.ReadKey().Key == ConsoleKey.O;
+            }
+            while (restart);
+
+            Console.WriteLine("Au revoir");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tests/ConsoleApp5/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool restart = false;
+            do
+            {
+                Console.WriteLine("Écrire un mot ou une phrase (Maximun 255 caractères)");
+                string saisie;
+                string voyelles = "aeiouyAEIOUY";
+                string consones = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ";
+                string space = " ";
+                string acents = "àáâèéêìíîòóôùúûýÀÁÂÈÉÊÌÍÎÒÓÔÙÚÛÝ";
+                int compCar = 0;
+                int voyCar = 0;
+                int conCar = 0;
+                int spaceC = 0;
+                int carAct = 0;
+                int speCar = 0;
+
+
+                saisie = Console.ReadLine();
+
+
+                for (int i = 0; i < saisie.Length; i++)//boucle for
+                {
+                    compCar++;
+                    char c = saisie[i];
+                    if (voyelles.Contains(c))
+                    {
+                        voyCar++;
+                    }
+                    if (consones.Contains(c))
+                    {
+                        conCar++;
+                    }
+                    if (space.Contains(c))
+                    {
+                        spaceC++;
+                    }
+                    if (acents.Contains(c))
+                    {
+                        carAct++;
+                    }
+                    speCar = compCar - voyCar - conCar - spaceC - carAct;
+                }
+                spaceC++;
+                if (compCar <= 1)//nombre de caractères
+                {
+                    Console.WriteLine("La ligne {0} compte {1} caractère", saisie, compCar);
+                }
+                else
+                {
+                    Console.WriteLine("La ligne {0} compte {1} caractères", saisie, compCar);
+                }
+
+                if (conCar <= 1)//nombre de consonnes
+                {
+                    Console.WriteLine("{0} consone", conCar);
+                }
+                else
+                {
+                    Console.WriteLine("{0} consonnes", conCar);
+                }
+
+                if (voyCar <= 1)//nombre de voyelles
+                {
+                    Console.WriteLine("{0} voyelle", voyCar);
+                }
+                else
+                {
+                    Console.WriteLine("{0} voyelles", voyCar);
+                }
+                if (spaceC <= 1)
+                {
+                    Console.WriteLine("{0} mot", spaceC);
+                }
+                else
+                {
+                    Console.WriteLine("{0} mots", spaceC);
+                }
+                if (carAct <= 1)
+                {
+                    Console.WriteLine("{0} caractère accentué", carAct);
+                }
+                else
+                {
+                    Console.WriteLine("{0} caractères accentués", carAct);
+                }
+                if(speCar <= 1)
+                {
+                    Console.WriteLine("{0} caractère spécial", speCar);
+                }
+                else
+                {
+                    Console.WriteLine("{0} caractères spéciaux", speCar);
+                }
+
+                Console.WriteLine("Voulez-vous refaire un nouveau ?");
+                restart = Console.ReadKey().Key == ConsoleKey.O;
+
+            }
+            while (restart);
+
+                
+            
+
+            Console.ReadLine();
+        }
+    }
+}
